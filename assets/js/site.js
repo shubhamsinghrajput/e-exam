d8fc681c85bb91a1948a60d14e49ff36922d9cf0
rishiraj-ldpl <rishiraj@mylinchpin.tech>
2019-01-12 15:15:14 +0530

    first push and commit

---
+++ assets/js/site.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $(".font-inc").click(function () {
+        var fz = $(".que-content").css("font-size");
+        var fzNo = parseInt(fz.substr(-4, 2));
+        if (fzNo < 35) {
+            fzNo += 1;
+            $(".que-content").css("font-size", fzNo + "px");
+        }
+    });
+    $(".font-dec").click(function () {
+        var fz = $(".que-content").css("font-size");
+        var fzNo = parseInt(fz.substr(-4, 2));
+        if (fzNo >= 11) {
+            fzNo += -1;
+            $(".que-content").css("font-size", fzNo + "px");
+        }
+    });
+    /*---------------------------------------*/
+    var ht = $(".top-right").html();
+    $(".que-viewport").prepend("<div class='top-right mb-top-right clearfix'> </div>").find(".top-right").prepend(ht);
+    /*---------------------------------------*/
+    
+    $(document).click(function () {
+        $('.que-viewport').animate({
+           "left" : "100%",
+        },function(){
+            $(this).hide(0);
+        });
+    });
+    $('.que-viewport').click(function (event) {
+        event.stopPropagation();
+    });
+    $(".btn-slide").click(function(){
+        $('.que-viewport').css("left","0");
+    });
+});
---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SSC and NDA</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <header class="header" id="header">
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <!-- Brand -->
+            <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt="logo.png"></a>
+         <!-- Navbar links -->
+            <div class="titile-time">
+                <h1 class="d-none d-sm-block">SSC - Sample Online Exam</h1>
+                <span class="remaining-time">Remaining Time :</span> <sapn class="view-time"> 00:25:18</sapn></span>
+            </div>
+            <div class="entry-fill">
+                <p class="mb-0">
+                    <span>Name</span>
+                    <span>: Rishiraj Sharma </span>
+                </p>
+                <p class="mb-0">
+                    <span>Roll no.</span>
+                    <span>: 154686 </span>
+                </p>
+                <p class="mb-0">
+                    <span>Exam Code</span>
+                    <span>: Sample </span>
+                </p>
+
+            </div>
+            <div class="top-right">
+                <div class="float-left pt-4">
+                    <p class="mb-0">
+                        <span>Lad No. </span>
+                        <span>: 1 </span>
+                    </p>
+                    <p class="mb-0">
+                        <span>Version </span>
+                        <span>: 1 </span>
+                    </p>
+                </div>
+                <div class="float-right">
+                    <figure class="pf-pic">
+                        <img src="assets/img/cand.jpg" alt="cand.jpg">
+                    </figure>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--===============CONTENT===============-->
+    <div class="content container-fluid" style="background:#fff;">
+        <form action="">
+            <div class="d-flex que-chart" style="margin:0 -15px">
+                <div class="pl-2 pr-2 q-con-wrapper">
+                    <div class="que-counter justify-content-between card c-card">
+                        <h2 class="mb-0">Question No: 1</h2>
+                        <ul class="right-listing">
+                            <li><a href="#" class="font-inc"><img src="assets/img/increase.png" alt=""></a></li>
+                            <li><a href="#" class="font-dec"><img src="assets/img/decrease.png" alt=""></a></li>
+                            <li>
+                                <select class="language">
+                                    <option class="dropdown-item" href="#">English</option>
+                                    <option class="dropdown-item" href="#">Hindi</option>
+                                </select>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="que-content p-1 p-sm-2 p-lg-4">
+                        <h2 class="question">
+                            9. What is a quote that inspires you?
+                        </h2>
+                        <ol class="que-option pt-2">
+                            <li>Go Hard or Go Home</li>
+            
+                            <li>Collect Memories Not Things</li>
+            
+                            <li>No Road is Long When You're In Good Company</li>
+            
+                            <li>Life Begins at The End Of Your Comfort Zone</li>
+                        </ol>
+                    </div>
+                    <button class="btn-slide"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                </div>
+                <aside class=" pl-2 pr-2 que-viewport" >
+                    <div class="que-type-list clearfix">
+                        <ul>
+                            <li><span>1</span>Questions Answered</li>
+                            <li><span>15</span>NOt Answered</li>
+                            <li><span>11</span>Review Answered</li>
+                            <li><span>20</span>Review Not Answered</li>
+                        </ul>
+                    </div>
+                    <ul class="que-no-list">
+                        <li><a href="#" class="">1</a></li>
+                        <li><a href="#" class="">2</a></li>
+                        <li><a href="#" class="">3</a></li>
+                        <li><a href="#" class="">4</a></li>
+                        <li><a href="#" class="">5</a></li>
+                        <li><a href="#" class="">6</a></li>
+                        <li><a href="#" class="">7</a></li>
+                        <li><a href="#" class="">8</a></li>
+                        <li><a href="#" class="">9</a></li>
+                        <li><a href="#" class="">10</a></li>
+                        <li><a href="#" class="">11</a></li>
+                        <li><a href="#" class="">12</a></li>
+                        <li><a href="#" class="">13</a></li>
+                        <li><a href="#" class="">14</a></li>
+                        <li><a href="#" class="">15</a></li>
+                        <li><a href="#" class="">16</a></li>
+                        <li><a href="#" class="">17</a></li>
+                        <li><a href="#" class="">18</a></li>
+                        <li><a href="#" class="">19</a></li>
+                        <li><a href="#" class="">20</a></li>
+                        <li><a href="#" class="">21</a></li>
+                        <li><a href="#" class="">22</a></li>
+                        <li><a href="#" class="">23</a></li>
+                        <li><a href="#" class="">24</a></li>
+                        <li><a href="#" class="">25</a></li>
+                        <li><a href="#" class="">26</a></li>
+                        <li><a href="#" class="">27</a></li>
+                        <li><a href="#" class="">28</a></li>
+                        <li><a href="#" class="">29</a></li>
+                        <li><a href="#" class="">30</a></li>
+                        <li><a href="#" class="">31</a></li>
+                        <li><a href="#" class="">32</a></li>
+                        <li><a href="#" class="">33</a></li>
+                        <li><a href="#" class="">34</a></li>
+                    </ul>
+                </aside>
+            </div>
+            <div class="btn-wrapper pt-3 pb-3">
+                 <button class="btn c-btn" role="button">Save &amp; Previous</button>
+                 <label for="opt-a" class="btn c-btn"><input type="radio" id="opt-a" name="option">A</label>
+                 <label for="opt-b" class="btn c-btn"><input type="radio" id="opt-b" name="option">B</label>
+                 <label for="opt-c" class="btn c-btn"><input type="radio" id="opt-c" name="option">C</label>
+                 <label for="opt-d" class="btn c-btn"><input type="radio" id="opt-d" name="option">D</label>
+                 <button class="btn c-btn" role="button">Save &amp; Next</button>
+                 <label for="m-for-rew" class="btn c-btn btn-yellow"><input type="checkbox" id="m-for-rew">Mark for Review</label>
+                 <button class="btn c-btn button-jamuni" role="button">Erase Answer</button>
+                 <button class="btn c-btn button-red" role="button">End Test</button>
+            </div>
+        </form>
+    </div>
+    <ul class="abc">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    <!--==================================================-->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/site.js"></script>
+</body>
+
+</html>
