b2c465a733c9212e1b28aa442f740c6eca9559eb
rishiraj-ldpl <rishiraj@mylinchpin.tech>
2019-01-23 10:52:41 +0530

    admin path change +

---
+++ admin/assets/js/site.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    $(".font-inc").click(function () {
+        var fz = $(".que-content").css("font-size");
+        var fzNo = parseInt(fz.substr(-4, 2));
+        if (fzNo < 35) {
+            fzNo += 1;
+            $(".que-content").css("font-size", fzNo + "px");
+        }
+    });
+    $(".font-dec").click(function () {
+        var fz = $(".que-content").css("font-size");
+        var fzNo = parseInt(fz.substr(-4, 2));
+        if (fzNo >= 11) {
+            fzNo += -1;
+            $(".que-content").css("font-size", fzNo + "px");
+        }
+    });
+    /*---------------------------------------*/
+    var ht = $(".top-right").html();
+    $(".que-viewport").prepend("<div class='top-right mb-top-right clearfix'> </div>").find(".top-right").prepend(ht);
+    /*---------------------------------------*/
+    //    function queChart() {
+    //        
+    //        $('.que-viewport').click(function (event) {
+    //            event.stopPropagation();
+    //        });
+    //        $(".btn-slide").click(function () {
+    //            var x = $(".que-viewport").offset().left;
+    //
+    //            if (x >= $(window).width()) {
+    //                $('.que-viewport').animate({
+    //                     "right": "0"
+    //                });
+    //            }
+    //        });
+    //        $(document).click(function () {
+    //            var x = $(".que-viewport").offset().left;
+    //            if ($(this).hasClass("btn-slide")) { 
+    //               
+    //            }
+    //            
+    //        });
+    //    }
+    //    queChart();
+
+
+
+    /*-----------------------------------------------*/
+    $(".btn-slide").click(function () {
+        var x = $(".que-viewport").offset().left;
+        if (x >= $(window).width()) {
+            $('.que-viewport').animate({
+                "right": "0"
+            });
+        }
+    });
+    $(".btn-slide-close").click(function () {
+        $('.que-viewport').animate({
+            "right": "-105%"
+        });
+
+    });
+    /*------------------------------------*/
+});
