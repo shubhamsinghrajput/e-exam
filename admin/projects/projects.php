79fe83f7cda03cf80ca3b5e2b96298c4dfa29d84
shubham <shubham@mylinchpin.tech>
2019-01-24 16:42:23 +0530

    and mobile nav fixed

--- admin/projects/projects.php
+++ admin/projects/projects.php
@@ -139,7 +139,11 @@
     <!-- Bootstrap core JavaScript-->
     <script src="../../assets/vendor/jquery/jquery.min.js"></script>
     <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- Custom scripts for all pages-->
+    <script src="../../assets/js/sb-admin.min.js"></script>
 
+ 
 
 </body>
 
