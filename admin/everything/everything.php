0dd93548bf97d60920d6b4d8def4ca5396ee23f6
rishiraj-ldpl <rishiraj@mylinchpin.tech>
2019-01-28 11:00:06 +0530

    28-01-19 pull and update

--- admin/everything/everything.php
+++ admin/everything/everything.php
@@ -22,6 +22,7 @@
 
     <!-- Custom styles for this template-->
     <link href="../../assets/css/sb-admin.css" rel="stylesheet">
+     <link href="../assets/css/global.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
  
 
