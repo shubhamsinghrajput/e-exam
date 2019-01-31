38d923578b755ae3d70b77a36180bcfad4a0554c
shubham <shubham@mylinchpin.tech>
2019-01-31 18:27:54 +0530

    project nav

--- admin/nav.php
+++ admin/nav.php
@@ -6,7 +6,7 @@
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="projects.php">Projects</a>
