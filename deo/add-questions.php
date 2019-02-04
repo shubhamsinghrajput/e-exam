a56e1980bf61247f1482c8c9029676663226eb56
shubham <shubham@mylinchpin.tech>
2019-02-04 17:43:44 +0530

    Add question

--- deo/add-questions.php
+++ deo/add-questions.php
@@ -39,11 +39,14 @@
         th { 
             text-align:center;
         }    
+        hr{
+            background: #ccc;
+        }
     </style>
 
 </head>
 
-<body id="page-top" class="pd-fix" style="background: grey">
+<body id="page-top" class="pd-fix">
     <?php include 'nav.php';?>
 
     <div id="wrapper">
@@ -86,37 +89,37 @@
                 <!--------------------------------------------------------------------------------->
                 <div class="col-lg-6">
                     <form autocomplete="off" class="form" role="form">
-                       <div class="form-group row">
-                        <label class="col-lg-3 col-form-label form-control-label">Name of the test</label>
-                        <div class="col-lg-9">
-                            <select class="form-control" id="user_time_zone" size="0">
-                                <option value="SSC">
-                                    SSC
-                                </option>
-                                <option value="AIIMS">
-                                    AIIMS
-                                </option>
-                                <option value="UPSC">
-                                    UPSC
-                                </option>
-                            </select>
-                        </div>
+                        <div class="form-group row">
+                            <label class="col-lg-3 col-form-label form-control-label">Name of the test</label>
+                            <div class="col-lg-9">
+                                <select class="form-control" id="user_time_zone" size="0">
+                                    <option value="SSC">
+                                        SSC
+                                    </option>
+                                    <option value="AIIMS">
+                                        AIIMS
+                                    </option>
+                                    <option value="UPSC">
+                                        UPSC
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                         <div class="form-group row">
-                           <label class="col-lg-3 col-form-label form-control-label">Subject</label>
-                           <div class="col-lg-9">
-                               <select class="form-control" id="user_time_zone" size="0">
-                                   <option value="English">
-                                       English
-                                   </option>
-                                   <option value="General Knowledge">
-                                       General Knowledge
-                                   </option>
-                                   <option value="Reasoning">
-                                       Reasoning
-                                   </option>
-                               </select>
-                           </div>
+                            <label class="col-lg-3 col-form-label form-control-label">Subject</label>
+                            <div class="col-lg-9">
+                                <select class="form-control" id="user_time_zone" size="0">
+                                    <option value="English">
+                                        English
+                                    </option>
+                                    <option value="General Knowledge">
+                                        General Knowledge
+                                    </option>
+                                    <option value="Reasoning">
+                                        Reasoning
+                                    </option>
+                                </select>
+                            </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-lg-3 col-form-label form-control-label">Difficulty Level</label>
@@ -136,64 +139,87 @@
                         </div>
                     </form>
                 </div>
-                <hr class="my-5">
-                <div class="col-lg-12 border-top-0">                   
-                   <div class="col-lg-6">
-                       <div class="form-group row">
-                           <label class="col-lg-3 col-form-label form-control-label">Type of Question</label>
-                           <div class="col-lg-9">
-                               <select class="form-control" id="user_time_zone" size="0">
-                                   <option value="Text">
-                                       Text
-                                   </option>
-                                   <option value="Image">
-                                       Image
-                                   </option>
-                               </select>
-                           </div>
-                       </div>
-                       <div class="form-group row">                         
-                           <label class="col-12 col-form-label form-control-label border border-secondary">Question No. : 01</label>
-                       </div>
-                           <div class="form-group row">
-                           <div class="col-12">
-                               <input class="form-control" type="text" placeholder="Please Enter Question Here">
-                           </div>
-                           
-                           <label class="col-1 col-form-label form-control-label">A</label>
-                           <div class="col-11">
-                               <input class="form-control" type="text" placeholder="Option A">
-                           </div>
-                           
-                           <label class="col-1 col-form-label form-control-label">B</label>
-                           <div class="col-11">
-                               <input class="form-control" type="text" placeholder="Option B">
-                           </div>
-                           
-                           <label class="col-1 col-form-label form-control-label">C</label>
-                           <div class="col-11">
-                               <input class="form-control" type="text" placeholder="Option C">
-                           </div>
-                           
-                           <label class="col-1 col-form-label form-control-label">D</label>
-                           <div class="col-11">
-                               <input class="form-control" type="text" placeholder="Option D">
-                           </div>
-                       </div>
-                   </div>
-                   
-                   <div class="col-lg-6">
-                       
-                   </div>
-                   
-                   
-                   
-                   
-                  
-               </div>
-                
-                
-                
+                <hr class="my-4">
+                <form autocomplete="off" class="form" role="form">
+                    <div class="col-lg-12 row">
+                        <div class="col-lg-7">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label form-control-label">Type of Question</label>
+                                <div class="col-lg-9">
+                                    <select class="form-control" id="user_time_zone" size="0">
+                                        <option value="Text">
+                                            Text
+                                        </option>
+                                        <option value="Image">
+                                            Image
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-12 col-form-label form-control-label border border-secondary">Question No. : 01</label>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <input class="form-control" type="text" placeholder="Please Enter Question Here" id="" name="">
+                                </div>
+
+                                <label class="col-1 col-form-label form-control-label">A</label>
+                                <div class="col-11">
+                                    <input class="form-control" type="text" placeholder="Option A" id="" name="">
+                                </div>
+
+                                <label class="col-1 col-form-label form-control-label">B</label>
+                                <div class="col-11">
+                                    <input class="form-control" type="text" placeholder="Option B" id="" name="">
+                                </div>
+
+                                <label class="col-1 col-form-label form-control-label">C</label>
+                                <div class="col-11">
+                                    <input class="form-control" type="text" placeholder="Option C" id="" name="">
+                                </div>
+
+                                <label class="col-1 col-form-label form-control-label">D</label>
+                                <div class="col-11">
+                                    <input class="form-control" type="text" placeholder="Option D" id="" name="">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-5">
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label form-control-label">Time</label>
+                                <div class="col-lg-9">
+                                    <input class="form-control" type="text" placeholder="Time" id="" name="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label form-control-label">Answer</label>
+                                <div class="col-lg-9">
+                                    <input class="form-control" type="text" placeholder="Answer" id="" name="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label form-control-label">Mark</label>
+                                <div class="col-lg-9">
+                                    <input class="form-control" type="text" placeholder="Mark" id="" name="">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-lg-3 col-form-label form-control-label">Explanation</label>
+                                <div class="col-lg-9">
+                                    <textarea class="form-control" id="explanation" name=""
+                                              rows="3" placeholder="Describe explanation here..."></textarea>
+                                </div>
+                            </div>
+                            
+                            <button class="btn btn-info btn-block" type="button">Submit</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
         <!-- /.container-fluid -->
@@ -214,7 +240,7 @@
         <i class="fas fa-angle-up"></i>
     </a>
     <!-- Add Project Modal-->
-    
+
     <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
